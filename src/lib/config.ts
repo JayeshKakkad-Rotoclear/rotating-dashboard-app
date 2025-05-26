0a515c366add62c7dd723610e253e3cfd6870b7e
JayeshKakkad-Rotoclear <jayesh.kakkad@rotoclear.com>
2025-05-26 21:12:51 +0200

    Test

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -7,8 +7,8 @@ export type Dashboard = {
 export const dashboards: Dashboard[] = [
   {
     name: 'Grafana - Inventory Panel 1',
-    url: 'http://localhost:3000/public/dashboard/4e106491-cd87-4d99-a530-988f7b9d5821',
-    duration: 10
+    url: 'http://10.50.9.16:3000/d-solo/felr9tnck43r4b/bestande-fertigteile-rotoclear-s3?orgId=1&from=now-30d&to=now&panelId=11&__feature.dashboardSceneSolo',
+    duration: 11
   },
   {
     name: 'Monday - Weekly Plan',
@@ -17,7 +17,7 @@ export const dashboards: Dashboard[] = [
   },
   {
     name: 'Salesforce - Delivery Overview',
-    url: 'http://10.50.9.16:3000/d-solo/felr9tnck43r4b/bestande-fertigteile-rotoclear-s3?orgId=1&from=now-30d&to=now&panelId=11&theme=light',
+    url: 'http://10.50.9.16:3000/d-solo/felr9tnck43r4b/bestande-fertigteile-rotoclear-s3?orgId=1&from=1745673437505&to=1748265437505&timezone=browser&panelId=11&__feature.dashboardSceneSolo',
     duration: 10
   }
   // Add more panels here
